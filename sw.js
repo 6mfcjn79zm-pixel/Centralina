05762902768edfdb832966f1c128a327988d648f
6mfcjn79zm-pixel <6mfcjn79zm@privaterelay.appleid.com>
2026-09-17 15:14:07 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'centralina-v2';
+const CORE_ASSET = './index.html';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['./', CORE_ASSET]).catch(() => {});
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) => {
+      return Promise.all(
+        names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Network-first: prova sempre a scaricare l'ultima versione online.
+// Solo se sei offline, usa la copia salvata come riserva.
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
